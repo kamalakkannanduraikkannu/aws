d58c998c8af09324f3cf60a207ad67ab03157588
kamalakkannanduraikkannu <139320501+kamalakkannanduraikkannu@users.noreply.github.com>
2023-09-09 12:06:36 +0800

    file discription added

--- aws-s3/test.py
+++ aws-s3/test.py
@@ -4,11 +4,14 @@ from botocore.client import Config
 B_name="coderbytechallengesandbox"
 F_prefix="__cb__"
 s3 = boto3.client('s3',config=Config(signature_version=UNSIGNED))
+// get the file names object based on prefix 
 res = s3.list_objects_v2(Bucket=B_name,Prefix=F_prefix)
 file_respose = res.get("Contents")
 file =[]
+// file names adding to list from dictionary 
 for fi in  file_respose:
   file.append(list(fi.values())[0])
+// read file condent from object "Body" 
 for f in file:
   res = s3.get_object(Bucket=B_name,Key=str(f))
   print(res.get("Body").read())
